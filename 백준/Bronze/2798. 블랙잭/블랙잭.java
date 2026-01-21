0555cc167a65f00f7007c2161bad8b35e44ff566
penep0 <102152521+penep0@users.noreply.github.com>
2026-01-21 18:19:02 +0900

    [Bronze II] Title: 블랙잭, Time: 100 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,26 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n;
-        int m;
-        int result = 0;
-        n = sc.nextInt();
-        m = sc.nextInt();
-        int[] a = new int[n];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] cards = new int[n];
+        int sum = 0;
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
+            cards[i] = Integer.parseInt(st.nextToken());
         }
-        for (int x = 0; x < n - 2; x++) {
-            for (int y = x + 1; y < n - 1; y++) {
-                for (int z = y + 1; z < n; z++) {
-                    if(a[x] + a[y] + a[z] <= m && a[x] + a[y] + a[z] > result) {
-                        result = a[x] + a[y] + a[z];
+
+        Arrays.sort(cards);
+
+        for (int i = 0; i < n - 2; i++) {
+            int start = i + 1;
+            int end = n - 1;
+            while (start < end) {
+                int cardSum = cards[i] + cards[start] + cards[end];
+                if (cardSum <= m) {
+                    if (cardSum >= sum) {
+                        sum = cardSum;
                     }
+                    start++;
+                } else {
+                    end--;
                 }
             }
         }
-        System.out.println(result);
+
+        System.out.println(sum);
     }
 }

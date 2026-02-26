3f483f39b992fd50f97da23e60af2b97dd247e8e
penep0 <102152521+penep0@users.noreply.github.com>
2026-02-26 10:28:45 +0900

    [Silver IV] Title: 괄호, Time: 192 ms, Memory: 18120 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        for(int i = 0; i < n; i++) {
+            String line = sc.nextLine();
+            Stack<Character> stack = new Stack<>();
+            boolean flag = true;
+            for(int j = 0; j < line.length(); j++) {
+                if(line.charAt(j) == '(') {
+                    stack.push(line.charAt(j));
+                } else if(!stack.isEmpty() && line.charAt(j) == ')') {
+                    stack.pop();
+                } else {
+                    flag = false;
+                    break;
+                }
+            }
+            if(!stack.isEmpty()) flag = false;
+            System.out.println(flag ? "YES" : "NO");
+        }
+    }
+}

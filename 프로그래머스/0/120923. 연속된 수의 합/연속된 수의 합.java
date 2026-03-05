d6f05a52370a18145d04d816ea0b317681cb3559
penep0 <102152521+penep0@users.noreply.github.com>
2026-03-05 18:37:31 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.03 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,19 +1,14 @@
 class Solution {
     public int[] solution(int num, int total) {
+        int sum=0;
+        int x;
+        for(int i=1;i<=num;i++){
+            sum+=i;
+        }
+        x=(total-sum)/num;
         int[] answer = new int[num];
-        
-        for (int i = -(total + num); i <= total + num; i++) {
-            int count = 0;
-            for (int j = i; j < i + num; j++) {
-                count += j;
-            }
-            if (count == total) {
-                int c = i;
-                for (int j = 0; j < num; j++) {
-                    answer[j] = c;
-                    c++;
-                }
-            }
+        for(int i=0;i<num;i++){
+            answer[i]=x+(i+1);
         }
         return answer;
     }
